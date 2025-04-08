e85f7b770e0061593d95221614e4ddc3c71376fd
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-04-08 10:01:00 +0200

    Add project files.

---
+++ CodeChallenge2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeChallenge2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ingredients = new List<string>();
+
+            ingredients.Add("200 g Hvedemel (ca. 3½ dl)");
+            ingredients.Add("2 teskefuld Bagepulver");
+            ingredients.Add("½ teskefuld Fint Salt");
+            ingredients.Add("2 Æg");
+            ingredients.Add("150g Sukker (ca. 1,75 dl)");
+            ingredients.Add("1 teskefuld Vaniljesukker");
+            ingredients.Add("125g Smeltet Smør");
+            ingredients.Add("2 Modne Bananer");
+            ingredients.Add("100g Mørk Chokolade");
+
+            Console.WriteLine("Banankage!!\n");
+
+            foreach (string ingredient in ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+        }
+    }
+}
